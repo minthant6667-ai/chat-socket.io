bee600a6ee0595dcb214023c02493f399ddf302a
Min Thant <minthant@gmail.com>
2026-08-31 11:44:03 +0630

    feat: improve chat message handling

--- public/main.js
+++ public/main.js
@@ -15,9 +15,9 @@ e.preventDefault()
 sendMessage()
 })
 
-// Update total clients 
-socket.on('clients-total', (data) => { 
-  clientsTotal.innerText = `${data} ${data === 1 ? 'user' : 'users'} online`
+// Update total clients
+socket.on('clients-total', (data) => {
+clientsTotal.innerText = `Total Clients: ${data}`
 })
 
 // Send message
