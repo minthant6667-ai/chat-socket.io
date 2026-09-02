a133b1c54fd9d917bb1d28925fc0807b23da0672
Min Thant <minthant@gmail.com>
2026-09-02 11:42:30 +0630

    Nothing

--- models/User.js
+++ models/User.js
@@ -1,24 +1,27 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      minlength: 3,
-      maxlength: 20,
-    },
-
-    password: {
-      type: String,
-      required: true,
-    },
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
   },
-  {
-    timestamps: true,
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
-)
+})
 
 module.exports = mongoose.model('User', userSchema)
